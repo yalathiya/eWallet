31611ee134f035d1cdb5340915a9c8e7924daf54
yalathiya <yashlathiya59ppsv@gmail.com>
2024-04-08 17:58:08 +0530

    Added extion method for model conversion ..

--- api/api_eWallet/api_eWallet/Static/Utility.cs
+++ api/api_eWallet/api_eWallet/Static/Utility.cs
@@ -1,5 +1,7 @@
 ﻿using api_eWallet.Services.Implementation;
 using api_eWallet.Services.Interfaces;
+using System.Reflection;
+using System.Text.Json.Serialization;
 
 namespace api_eWallet.Static
 {
@@ -24,6 +26,43 @@ namespace api_eWallet.Static
             services.AddSingleton<ISender, EmailService>();
 
             return services; // This allows chaining of registrations
+        }
+
+        /// <summary>
+        /// Converts properties of a source model to properties of a target model based on JSON property names.
+        /// </summary>
+        /// <typeparam name="T">The type of the target model.</typeparam>
+        /// <param name="sourceModel">The source model whose properties are to be converted.</param>
+        /// <returns>A new instance of the target model with properties populated from the source model.</returns>
+        public static T ConvertModel<T>(this object sourceModel)
+        {
+            // generates blank instance of type T
+            T targetModel = Activator.CreateInstance<T>();
+
+            // Type contains className, all properties within class, etc..
+            Type sourceType = sourceModel.GetType();
+            Type targetType = typeof(T);
+
+            // Get source properties with JSON property name attributes
+            PropertyInfo[] sourceProps = sourceType.GetProperties()
+                                                   .Where(prop => prop.IsDefined(typeof(JsonPropertyNameAttribute), false))
+                                                   .ToArray();
+
+            // Iterate through source properties
+            foreach (PropertyInfo prop in sourceProps)
+            {
+                // Get the JSON property name from the attribute
+                JsonPropertyNameAttribute attribute = (JsonPropertyNameAttribute)Attribute.GetCustomAttribute(prop, typeof(JsonPropertyNameAttribute));
+                string targetPropName = attribute.Name;
+
+                // Find corresponding property in target model
+                PropertyInfo targetPropertyInfo = targetType.GetProperty(targetPropName);
+
+                // Set the value of the target property from the source property
+                targetPropertyInfo.SetValue(targetModel, prop.GetValue(sourceModel));
+            }
+
+            return targetModel;
         }
 
         #endregion
