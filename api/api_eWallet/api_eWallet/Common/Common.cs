22b4e053e81bad4c4328fd813789d700f205161c
yalathiya <yashlathiya59ppsv@gmail.com>
2024-04-08 16:28:45 +0530

    Added Enum

---
+++ api/api_eWallet/api_eWallet/Common/Common.cs
@@ -0,0 +1,22 @@
+﻿namespace api_eWallet.Common
+{
+    /// <summary>
+    /// Currency
+    /// </summary>
+    public enum Currency
+    {
+        INR,
+        YEN,
+        USD
+    }
+
+    /// <summary>
+    /// Transaction Type
+    /// </summary>
+    public enum TransactionType
+    {
+        Deposit,
+        Transfer,
+        Withdraw
+    }
+}
