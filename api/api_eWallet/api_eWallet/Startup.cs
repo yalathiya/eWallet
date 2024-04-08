44e0c235e317b2a62a5e4be35e5ab5a4697b2d0a
yalathiya <yashlathiya59ppsv@gmail.com>
2024-04-08 11:57:56 +0530

    project init

---
+++ api/api_eWallet/api_eWallet/Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace api_eWallet
+{
+    public class Startup
+    {
+        #region Public Members 
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+
+        #endregion
+    }
+}
