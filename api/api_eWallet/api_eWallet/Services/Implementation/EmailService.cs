8a30894cf2f390ca997f4d99234f69644e478a08
yalathiya <yashlathiya59ppsv@gmail.com>
2024-04-09 09:51:52 +0530

    Email Service implemented

--- api/api_eWallet/api_eWallet/Services/Implementation/EmailService.cs
+++ api/api_eWallet/api_eWallet/Services/Implementation/EmailService.cs
@@ -1,4 +1,6 @@
 ﻿using api_eWallet.Services.Interfaces;
+using System.Net.Mail;
+using System.Net;
 
 namespace api_eWallet.Services.Implementation
 {
@@ -10,11 +12,36 @@ namespace api_eWallet.Services.Implementation
         /// <summary>
         /// Sends Email to user 
         /// </summary>
+        /// <param name="recipientEmail"> recipientEmail </param>
         /// <param name="message"> message </param>
-        /// <exception cref="NotImplementedException"></exception>
         public void Send(string message)
         {
-            throw new NotImplementedException();
+            try
+            {
+                // sender details
+                string senderEmail = "eWallet.notify.user@outlook.com";
+                string senderPassword = "eWallet@7777";
+
+                // fetch from jwt
+                string recipientEmail = "";
+
+                MailMessage mail = new MailMessage(senderEmail, recipientEmail);
+                mail.Subject = "eWallet Notification";
+                mail.Body = message;
+
+                SmtpClient smtpClient = new SmtpClient("smtp-mail.outlook.com");
+                smtpClient.Port = 587;
+                smtpClient.Credentials = new NetworkCredential(senderEmail, senderPassword);
+                smtpClient.EnableSsl = true;
+
+                smtpClient.Send(mail);
+
+                Console.WriteLine("Registration email sent successfully!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending registration email: {ex.Message}");
+            }
         }
     }
 }
